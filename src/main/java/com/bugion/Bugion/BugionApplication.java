e23e061d02853f4a61bd361c83794ee43c89da6d
unknown <tran.n.cam@gmail.com>
2023-02-02 21:16:07 +0100

    First commit

---
+++ src/main/java/com/bugion/Bugion/BugionApplication.java
@@ -0,0 +1,13 @@
+package com.bugion.Bugion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BugionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BugionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bugion/Bugion/BugionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bugion.Bugion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BugionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
